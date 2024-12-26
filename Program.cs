3b29d7da8b22280d0409caafaf415d9a69e30a69
asikharulidze <ana.sixarulidze@gmail.com>
2024-12-26 15:22:23 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ref_keyword ;
+
+class Program{
+    
+        static void Main(string[] args)
+        {
+            
+            int a = 10, b = 12;
+            
+            Console.WriteLine("Initial value of a is {0}", a);
+            Console.WriteLine("Initial value of b is {0}", b);
+            Console.WriteLine();
+            // Ref-is gareshe methodi
+            addValue(a);
+            Console.WriteLine("Value of a after method: {0}", a);
+
+            // methodis gamozaxeba Ref-it
+            addValue(ref b);
+            Console.WriteLine("Value of b after Ref Method: {0}", b);
+        }
+        public static void addValue(int a)
+        {
+            a += 10;
+        }
+        
+        public static void addValue(ref int b)
+        {
+            b += 5;
+        }
+    }
+
